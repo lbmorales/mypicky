2e3afd11625562ad4d37bb37d19b72e6afcb61cb
Lilibet Borges Morales <lilibet.borges@gmail.com>
2017-12-26 11:51:00 +0100

    Add restaurants template

--- restaurants/models.py
+++ restaurants/models.py
@@ -6,3 +6,6 @@ class RestaurantLocation(models.Model):
     categories  = models.CharField(max_length=120, null=True, blank=True)
     created_at  = models.DateTimeField(auto_now_add=True)
     updated_at  = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
--- restaurants/views.py
+++ restaurants/views.py
@@ -1,19 +1,12 @@
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
 
-class ContactView(TemplateView):
-    template_name = "contact.html"
+from .models import RestaurantLocation
 
-class HomeView(TemplateView):
-    template_name = "home.html"
-
-class AboutView(TemplateView):
-    template_name = "about.html"
-
-
-'''
-class ContactView(View):
-    def get(self, request, *args, **kwargs):
-        context = {}
-        return render(request, "contact.html", context)
-'''
+def restaurant_listview(request):
+    template_name = "restaurants/restaurants_list.html"
+    queryset = RestaurantLocation.objects.all()
+    context = {
+        "object_list": queryset
+    }
+    return render(request, template_name, context)
