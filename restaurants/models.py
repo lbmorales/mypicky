bed89defd03f22b9579f7eb79a6acd4f63ddb26f
Lilibet Borges Morales <lilibet.borges@gmail.com>
2017-12-28 15:33:17 +0100

    Add details view to restaurants

--- restaurants/models.py
+++ restaurants/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class RestaurantLocation(models.Model):
     name        = models.CharField(max_length=120)
     location    = models.CharField(max_length=120, null=True, blank=True)
-    categories  = models.CharField(max_length=120, null=True, blank=True)
+    category  = models.CharField(max_length=120, null=True, blank=True)
     created_at  = models.DateTimeField(auto_now_add=True)
     updated_at  = models.DateTimeField(auto_now=True)
 
--- restaurants/views.py
+++ restaurants/views.py
@@ -1,12 +1,29 @@
-from django.shortcuts import render
-from django.views.generic.base import TemplateView
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import TemplateView, ListView, DetailView
 
+from django.db.models import Q
 from .models import RestaurantLocation
 
-def restaurant_listview(request):
-    template_name = "restaurants/restaurants_list.html"
+class RestaurantListView(ListView):
+    def get_queryset(self):
+        slug = self.kwargs.get("slug")
+
+        if slug:
+            queryset = RestaurantLocation.objects.filter(
+                Q(category__iexact=slug) |
+                Q(category__icontains=slug)
+            )
+        else:
+            queryset = RestaurantLocation.objects.all()
+        
+        return queryset
+
+class RestaurantDetailView(DetailView):
+
     queryset = RestaurantLocation.objects.all()
-    context = {
-        "object_list": queryset
-    }
-    return render(request, template_name, context)
+
+    #capture custom id name from url
+    def get_object(self, *args, **kwargs):
+        rest_id = self.kwargs.get('rest_id')
+        obj = get_object_or_404(RestaurantLocation, id=rest_id)
+        return obj
