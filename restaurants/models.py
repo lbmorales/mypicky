65388349b102f330a7cf0269ad39032fa280eed0
Lilibet Borges Morales <lilibet.borges@gmail.com>
2017-12-28 16:49:49 +0100

    Create a unique slug generator module for retaurant model

--- restaurants/models.py
+++ restaurants/models.py
@@ -3,9 +3,10 @@ from django.db import models
 class RestaurantLocation(models.Model):
     name        = models.CharField(max_length=120)
     location    = models.CharField(max_length=120, null=True, blank=True)
-    category  = models.CharField(max_length=120, null=True, blank=True)
+    category    = models.CharField(max_length=120, null=True, blank=True)
     created_at  = models.DateTimeField(auto_now_add=True)
     updated_at  = models.DateTimeField(auto_now=True)
+    slug        = models.SlugField(null=True, blank=True)
 
     def __str__(self):
         return self.name
